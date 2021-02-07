8e4952d364de89167242ec694ba10873f3ffa6ce
Theres Mary <theres.mec@gmail.com>
2021-02-07 23:00:08 +0530

    test file

--- complex_test.cpp
+++ complex_test.cpp
@@ -4,21 +4,25 @@
 
 TEST(Complex,EmptyConstructor)
 {
-    Complex <int> a;
-    EXPECT_EQ(0, a.real());
-    EXPECT_EQ(0, a.imag());
+    Complex <int> C1;
+    EXPECT_EQ(0, C1.real());
+    EXPECT_EQ(0, C1.imag());
 }
 
 TEST(Complex,ParameterizedConstructor)
 {
-    Complex <int> a(6,9);
-    EXPECT_EQ(6,a.real());
-    EXPECT_EQ(9,a.imag());
+    Complex <int> C1(6,9);
+    EXPECT_EQ(6,C1.real());
+    EXPECT_EQ(9,C1.imag());
 }
 
 TEST(Complex,CopyConstructor)
 {
-    Complex <int> a(6,9);
-    EXPECT_EQ(6,a.real());
-    EXPECT_EQ(9,a.imag());
+    Complex <int> C1(6,9);
+    EXPECT_EQ(6,C1.real());
+    EXPECT_EQ(9,C1.imag());
+
+    Complex C2(C1);
+    EXPECT_EQ(6,C2.real());
+    EXPECT_EQ(9,C2.imag());
 }
