9a4d6a854fd2de24f80027da4b4f06e42a3c06dc
Theres Mary <theres.mec@gmail.com>
2021-02-07 22:57:46 +0530

    cpp file

--- complex.cpp
+++ complex.cpp
@@ -1,31 +1,57 @@
-include"complex.h"
+#include"complex.h"
 #include <iostream>
-
 using namespace std;
 
+/**
+ * @brief Constructed Complex< T>:: Complex object
+ * 
+ * @tparam T 
+ */
 template<typename T>
-Complex<T>::Complex():m_real(0),m_image(0)
+Complex<T>::Complex():m_real(0),m_image(0){}
+
+/**
+ * @brief Constructed Complex< T>:: Complex object
+ * 
+ * @tparam T 
+ * @param x1 
+ * @param y1 
+ */
+template<typename T>
+Complex<T>::Complex(T x1,T y1):m_real(x1),m_image(y1)
 {
 }
 
-template<typename T>
-Complex<T>::Complex(T a,T b):m_real(a),m_image(b)
-{
-}
-
-template<typename T>
+/**
+ * @brief Function to return real no
+ * 
+ * @tparam T 
+ * @return T 
+ */
+template <typename T>
 T Complex<T>::re()
 {
     return m_real;
 }
 
-template <class T>
+/**
+ * @brief Function to return imaginary no
+ * 
+ * @tparam T 
+ * @return T 
+ */
+template <typename T>
 T Complex<T>::img()
 {
     return m_image;
 }
 
-template <class T>
+/**
+ * @brief Function to display complex no
+ * 
+ * @tparam T 
+ */
+template <typename T>
 void Complex<T>::display()
 {
     cout<<re()<<img();
