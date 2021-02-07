d73051ebd524b573da22f2802502db6356dd6457
Theres Mary <theres.mec@gmail.com>
2021-02-07 22:59:48 +0530

    header file

--- complex.h
+++ complex.h
@@ -1,3 +1,13 @@
+/**
+ * @file complex
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __COMPLEX_H
 #define __COMPLEX_H
 
@@ -5,10 +15,14 @@ template<typename T>
 class Complex {
   T m_real;
   T m_image;
+
   public:
+
   Complex();
   Complex(T,T);
   void display();
+  T re();
+  T img();
 };
 
 #endif
